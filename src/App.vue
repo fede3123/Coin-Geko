43a214f8e77160c2c08d16e81df58e95c1eaa33f
federico <federik4312@gmail.com>
2022-03-06 10:35:27 -0500

    complete all project and learn how use the basic things in vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,72 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="container">
+    <div class="row">
+      <h1>Coin market</h1>
+
+        <input type="text" class="form-control bg-dark text-light rounded-0 border-0 my-4"
+        placeholder="Search a coin"
+        @keyup="searchCoins()"
+        v-model="textSearch">
+        <table class="table table-dark">
+          <thead>
+            <tr>
+              <th v-for="title in titles" :key="title">
+                {{title}}
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(coin, index) in filteredCoins" :key="coin.id">
+              <td class="text-muted">
+                {{index +1}}
+              </td>
+              <td>
+                <img :src="coin.image" style="width:2rem" class="me-2">
+                <span>{{coin.name}}</span>
+                <span class="ms-2 text-uppercase text-muted">{{coin.symbol}}</span>
+              </td>
+              <td>
+                ${{coin.current_price}}
+              </td>
+              <td :class="[coin.price_change_percentage_24h > 0 ? 'text-success': 'text-danger']">
+                {{coin.price_change_percentage_24h}} %
+              </td>
+              <td>
+                ${{coin.total_volume.toLocaleString()}}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+    </div>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script>
+  export default {
+    name: "app",
+    data(){
+      return{
+        coins: [],
+        filteredCoins:[],
+        titles: ['#', 'Coin', 'Price', 'Price Change', '24h Volume'],
+        textSearch: ''
+
+      }
+    },
+    async mounted() {
+      const res = await fetch(  'https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false' )
+      const data = await res.json()
+      console.log(data)
+      this.coins = data
+      this.filteredCoins = data
+    },
+    methods: {
+      searchCoins() {
+        this.filteredCoins = this.coins.filter(coin =>
+            coin.name.toLowerCase().includes(this.textSearch.toLowerCase()) ||
+            coin.symbol.toLowerCase().includes(this.textSearch.toLowerCase()));
+      },
+    },
+  };
+
+</script>
